ea25f46e45b353a925e63344b58a80252d9d8e8d
obstruse <neisius2@earthlink.net>
2018-01-15 07:53:55 -0800

    Update camera.py

--- python/camera.py
+++ python/camera.py
@@ -6,18 +6,12 @@ from pygame.locals import *
 import os
 from PIL import Image
 
-os.putenv('SDL_FBDEV', '/dev/fb1')
-os.putenv('SDL_VIDEODRIVER','fbcon')
-os.putenv('SDL_MOUSEDRV', 'TSLIB')
-os.putenv('SDL_MOUSEDEV', '/dev/input/touchscreen')
-
-import RPi.GPIO as GPIO
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
-GPIO.setup(18, GPIO.OUT)
-GPIO.output(18, True)
+# if user is root, then output to fb1
+if os.geteuid() == 0:
+	os.putenv('SDL_FBDEV', '/dev/fb1')
+	os.putenv('SDL_VIDEODRIVER','fbcon')
+	os.putenv('SDL_MOUSEDRV', 'TSLIB')
+	os.putenv('SDL_MOUSEDEV', '/dev/input/touchscreen')
 
 pygame.init()
 pygame.camera.init()
@@ -47,6 +41,4 @@ while going:
 	#image = image.convert('1', dither=Image.NONE)
 	lcd.blit(image, (0,0))
 	pygame.display.flip()
-
-#GPIO.output(18, False)
 
