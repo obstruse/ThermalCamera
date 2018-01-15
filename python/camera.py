8eefac761f3b14612b80fc94b031b6a2f460f3d5
obstruse <neisius2@earthlink.net>
2018-01-15 08:58:57 -0800

    Disable mouse for root

--- python/camera.py
+++ python/camera.py
@@ -6,16 +6,19 @@ from pygame.locals import *
 import os
 from PIL import Image
 
+
 # if user is root, then output to fb1
 if os.geteuid() == 0:
 	os.putenv('SDL_FBDEV', '/dev/fb1')
 	os.putenv('SDL_VIDEODRIVER','fbcon')
 	os.putenv('SDL_MOUSEDRV', 'TSLIB')
 	os.putenv('SDL_MOUSEDEV', '/dev/input/touchscreen')
-	pygame.mouse.set_visible(False)
 
 pygame.init()
 pygame.camera.init()
+
+if os.geteuid() == 0:
+	pygame.mouse.set_visible(False)
 
 lcd = pygame.display.set_mode((320, 240))
 
@@ -30,16 +33,14 @@ while running:
 	events = pygame.event.get()
 	for e in events:
 		if (e.type is MOUSEBUTTONDOWN):
-#			cam.stop()
 			running = False
 
 		if e.type == QUIT or (e.type == KEYDOWN and e.key == K_ESCAPE):
-#			cam.stop()
 			running = False
 
 	if cam.query_image():
 		image = cam.get_image()
-	#image = image.convert('1', dither=Image.NONE)
+
 	lcd.blit(image, (0,0))
 	pygame.display.flip()
 
